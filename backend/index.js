5df825487860b2cfbf561d1c2a2b730ace98b2e6
eg337 <eg6322@gmail.com>
2024-04-20 01:57:29 -0400

    boiler plate

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,31 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = 8080;
 
-app.listen(
-    PORT,
-    () => console.log(`it's alive on http://localhost:${PORT}`)
-)
+app.use( express.json() )
+
+app.get('/tshirt', (req,res) =>{
+    res.status(200).send({
+        tshirt: 'testing tesitng',
+        size: 'large'
+    })
+});
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+    const { test_var } = req.body;
+
+    if (!logo) {
+        res.status(418).send({message: "We need a logo!"})
+    }
+
+    res.send({
+        tshirt: `shirt with your ${logo} and ID of ${id} and test_var of ${test_var}`,
+    })
+});
+
+
+
+
+app.listen(PORT)
