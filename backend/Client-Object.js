a7aa7654ae494fe1b1e5a99f157e1df3f1898fff
Arjun-R24 <arjun.rajaram2404@gmail.com>
2024-04-20 14:41:10 -0400

    arjun branch

---
+++ backend/Client-Object.js
@@ -0,0 +1,10 @@
+class Client_Object {
+    constructor(intro, radius, lon, lat, answer, offer) {
+        this.intro = intro;
+        this.radius = radius;
+        this.lon = lon;
+        this.lat = lat; 
+        this.answer = answer;
+        this.offer = offer; 
+    }
+}
---
+++ frontend/src/components/OfferPopUp/OfferPopUp.js
@@ -0,0 +1,6 @@
+function OfferPopUp() {
+    return (
+        <>
+        </>
+    );
+}
