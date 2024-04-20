381d63a367cf35d4bbf8c5e19d3b5af1a3312dec
John Kim <johnkim@Johns-MacBook-Pro-8.local>
2024-04-20 19:02:02 -0400

    cors

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,12 @@ const app = express();
 const PORT = 8080;
 
 app.use( express.json() )
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
 
 app.listen(
     PORT,
